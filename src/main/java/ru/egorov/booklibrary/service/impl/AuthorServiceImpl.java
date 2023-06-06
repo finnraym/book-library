7aee8531f42a3b668d51f9073cf37ba8f1503243
Vladimir Egorov <vpegorov95@gmail.com>
2023-06-06 13:59:59 +0700

    implementation AuthorService

--- src/main/java/ru/egorov/booklibrary/service/impl/AuthorServiceImpl.java
+++ src/main/java/ru/egorov/booklibrary/service/impl/AuthorServiceImpl.java
@@ -2,32 +2,49 @@ package ru.egorov.booklibrary.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.egorov.booklibrary.domain.entity.Author;
+import ru.egorov.booklibrary.exception.DataNotFoundException;
 import ru.egorov.booklibrary.repository.AuthorRepository;
 import ru.egorov.booklibrary.service.AuthorService;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
+    @Transactional(readOnly = true)
     @Override
     public Author getById(Long id) {
-        return null;
+        return authorRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Author with id " + id + " not found."));
     }
 
+    @Transactional
     @Override
     public void saveNewAuthor(Author author) {
-
+        authorRepository.save(author);
     }
 
+    @Transactional
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        Author updatedAuthor = getById(author.getId());
+
+        updatedAuthor.setFirstName(author.getFirstName());
+        updatedAuthor.setSecondName(author.getSecondName());
+
+        if (Objects.nonNull(author.getDateOfBirth()))
+            updatedAuthor.setDateOfBirth(author.getDateOfBirth());
+
+        return updatedAuthor;
     }
 
+    @Transactional
     @Override
     public void deleteAuthorById(Long id) {
-
+        authorRepository.deleteById(id);
     }
 }
